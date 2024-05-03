f1650308edb6e779a41d8809c9cb0388bec48bad
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-03 15:11:38 +0530

    created the basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <main>
-    
+  return <main className="nav_container">
+    <h1 className="nav_title">
+      TITLE
+    </h1>
+    <ul>
+      <li>HOME</li>
+      <li>FEATURES</li>
+      <li>TESTIMONIALS</li>
+      <li>REGISTER</li>
+      <li>LOGIN</li>
+    </ul>
   </main>;
 };
 export default Navbar;
