d59a8a524708b74070f1c89d10c879722a4f4164
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-03 15:48:37 +0530

    made navbar completely responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
 import "./Navbar.css";
+import close_icon from "../assets/close_icon.svg";
+import hamburger_icon from "../assets/hamburger_icon.svg";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <main className="nav_container">
-    <h1 className="nav_title">
-      TITLE
-    </h1>
-    <ul>
-      <li>HOME</li>
-      <li>FEATURES</li>
-      <li>TESTIMONIALS</li>
-      <li>REGISTER</li>
-      <li>LOGIN</li>
-    </ul>
-  </main>;
+  const [showMenu, setShowMenu] = useState(() => false);
+
+  return (
+    <main className="nav_container">
+      <h1 className="nav_title">TITLE</h1>
+      {!showMenu ? (
+        <img
+          src={hamburger_icon}
+          className="hamburger_icon"
+          onClick={() => setShowMenu(() => true)}
+        />
+      ) : (
+        <img
+          src={close_icon}
+          className="close_icon"
+          onClick={() => setShowMenu(() => false)}
+        />
+      )}
+      <ul className={!showMenu && "hidden"}>
+        <li>HOME</li>
+        <li>FEATURES</li>
+        <li>TESTIMONIALS</li>
+        <li>REGISTER</li>
+        <li>LOGIN</li>
+      </ul>
+    </main>
+  );
 };
 export default Navbar;
