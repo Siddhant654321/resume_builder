05273435555ab86e18315fe3f92a6823864a0e92
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-04 15:30:17 +0530

    created the features section

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,10 +1,39 @@
-import './FeatureSection.css';
+import "./FeatureSection.css";
 
 const FeatureSection = () => {
+  const features = [
+    {
+      heading: "FREE TO USE",
+      description:
+        "Our resume builder is completely free to use, empowering you to create and download resumes without worrying about any fees or hidden charges.",
+    },
+    {
+      heading: "ATS COMPLIANT",
+      description:
+        "Increase your job application success with resumes optimized for Applicant Tracking Systems (ATS), ensuring your profile gets noticed by employers.",
+    },
+    {
+      heading: "MULTIPLE TEMPLATES",
+      description: "Our resume builder offers you a curated list of templates to choose your next great resume. Every template has been created with your success in mind.",
+    },
+    {
+      heading: "BEST QUALITY RESUME",
+      description:
+        "We make sure that your next resume is designed to highlight your skills and experiences with clarity, precision, and visual appeal setting you apart from the competition.",
+    },
+  ];
   return (
-    <div>
-        
+    <div className="feature_section">
+    <h1 className="features_main_heading">FEATURES</h1>
+      <div className="feature_container">
+        {features.map((feature_data, index) => (
+          <div className="feature_box" key={index}>
+            <p className="feature_subheading">{feature_data.heading}</p>
+            <p className="feature_description">{feature_data.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
-export default FeatureSection
+  );
+};
+export default FeatureSection;
