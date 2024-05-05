b820cfcd6b8692fef18f2ebc4cc0e6a0a1b10cda
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-05 16:39:54 +0530

    created the footer section

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -2,9 +2,31 @@ import "./FooterSection.css";
 
 const FooterSection = () => {
   return (
-    <div>
-
+    <div className="footer_section">
+      <div className="top_section">
+        <h1>TITLE</h1>
+        <div className="right_section">
+          <div>
+            <h3>PAGES</h3>
+            <ul>
+              <li>HOME</li>
+              <li>LOGIN</li>
+              <li>SIGNUP</li>
+            </ul>
+          </div>
+          <div>
+            <h3>LEGAL</h3>
+            <ul>
+              <li>PRIVACY POLICY</li>
+              <li>TERMS AND CONDITIONS</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="bottom_section">
+        <p>Copyright © <span>Title™</span> All Rights Reserved</p>
+      </div>
     </div>
-  )
-}
-export default FooterSection
+  );
+};
+export default FooterSection;
