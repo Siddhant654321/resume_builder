a8a5f8a3a76f18b570a1581a0f3dcc073322c47b
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-05 20:06:10 +0530

    added react router's Link component to navbar and footer links

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,30 +1,43 @@
+import { Link } from "react-router-dom";
 import "./FooterSection.css";
 
 const FooterSection = () => {
   return (
     <div className="footer_section">
       <div className="top_section">
-        <h1>TITLE</h1>
+        <h1><Link to="/">TITLE</Link></h1>
         <div className="right_section">
           <div>
             <h3>PAGES</h3>
             <ul>
-              <li>HOME</li>
-              <li>LOGIN</li>
-              <li>SIGNUP</li>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <Link to="/login">LOGIN</Link>
+              </li>
+              <li>
+                <Link to="/signup">SIGNUP</Link>
+              </li>
             </ul>
           </div>
           <div>
             <h3>LEGAL</h3>
             <ul>
-              <li>PRIVACY POLICY</li>
-              <li>TERMS AND CONDITIONS</li>
+              <li>
+                <Link to="privacy-policy">PRIVACY POLICY</Link>
+              </li>
+              <li>
+                <Link to="terms-and-conditions">TERMS AND CONDITIONS</Link>
+              </li>
             </ul>
           </div>
         </div>
       </div>
       <div className="bottom_section">
-        <p>Copyright © <span>Title™</span> All Rights Reserved</p>
+        <p>
+          Copyright © <span>Title™</span> All Rights Reserved
+        </p>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.css";
 import close_icon from "../assets/close_icon.svg";
 import hamburger_icon from "../assets/hamburger_icon.svg";
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(() => false);
@@ -10,29 +10,39 @@ const Navbar = () => {
   return (
     <>
       <main className="nav_container">
-      <h1 className="nav_title">TITLE</h1>
-      {!showMenu ? (
-        <img
-          src={hamburger_icon}
-          className="hamburger_icon"
-          onClick={() => setShowMenu(() => true)}
-        />
-      ) : (
-        <img
-          src={close_icon}
-          className="close_icon"
-          onClick={() => setShowMenu(() => false)}
-        />
-      )}
-      <ul className={!showMenu && "hidden"}>
-        <li>HOME</li>
-        <li>FEATURES</li>
-        <li>TESTIMONIALS</li>
-        <li>REGISTER</li>
-        <li>LOGIN</li>
-      </ul>
-    </main>
-    <Outlet />
+        <h1 className="nav_title"><Link to="/">TITLE</Link></h1>
+        {!showMenu ? (
+          <img
+            src={hamburger_icon}
+            className="hamburger_icon"
+            onClick={() => setShowMenu(() => true)}
+          />
+        ) : (
+          <img
+            src={close_icon}
+            className="close_icon"
+            onClick={() => setShowMenu(() => false)}
+          />
+        )}
+        <ul className={!showMenu && "hidden"}>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to='/#features'>FEATURES</Link>
+          </li>
+          <li>
+            <Link to="/#testimonials">TESTIMONIALS</Link>
+          </li>
+          <li>
+            <Link to="/signup">SIGNUP</Link>
+          </li>
+          <li>
+            <Link to="/login">LOGIN</Link>
+          </li>
+        </ul>
+      </main>
+      <Outlet />
     </>
   );
 };
