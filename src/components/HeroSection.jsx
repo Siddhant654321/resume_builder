f162ccc425bd087da6ee26e9a647c825dc31f807
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-05 14:19:15 +0530

    made changes to hero section to make it easier to develop the bottom section of the homepage

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,14 +5,14 @@ import BlurredSVG from './BlurredSVG';
 const HeroSection = () => {
   return (
     <main className='hero_section_container'>
-      <BlurredSVG className='blurred_svg ellipse_1' />
-      <BlurredSVG className='blurred_svg ellipse_2' width={786} height={682} />
-      <BlurredSVG className='blurred_svg ellipse_3' width={847} height={564} />
+      <BlurredSVG className='blurred_svg hero_section_ellipse_1' />
+      <BlurredSVG className='blurred_svg hero_section_ellipse_2' width={786} height={682} />
+      <BlurredSVG className='blurred_svg hero_section_ellipse_3' width={847} height={564} />
       <div className='first_child'>
         <p>Hi There!</p>
         <h1><span className='purple_text'>TITLE</span> BRINGS YOU THE BEST RESUME</h1>
         <p>Getting a job has never been more easier before.</p>
-        <button>Get Started</button>
+        <button className='get_started_button'>Get Started</button>
       </div>
       <div>
         <img src={hero_image} alt="" />
