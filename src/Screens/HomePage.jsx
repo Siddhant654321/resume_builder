1f5cc4936cd8bb7cd6acabbccea576d096d0a247
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-05 19:48:00 +0530

    created homepage screen

---
+++ src/Screens/HomePage.jsx
@@ -0,0 +1,16 @@
+import FeatureSection from "./components/FeatureSection"
+import HeroSection from "./components/HeroSection"
+import HomePageBottom from "./components/HomePageBottom"
+import TestimonialsSection from "./components/TestimonialsSection"
+
+const Homepage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeatureSection />
+      <TestimonialsSection />
+      <HomePageBottom />
+    </div>
+  )
+}
+export default Homepage
