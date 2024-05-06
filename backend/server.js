38a7b4d6ad5cc2c5a06a2f3156748e68d3dcca48
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-06 15:12:17 +0530

    setup cors in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, 
+  credentials: true
+};
 
 const app = express();
+
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.send('Welcome')
