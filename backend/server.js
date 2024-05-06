12337ab70ba343cb4ab16f4dc000d819b87a95f3
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-06 15:13:35 +0530

    added body parser middleware and cookie parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL, 
@@ -11,6 +12,13 @@ const app = express();
 
 app.options('*', cors(corsOptions));
 app.use(cors(corsOptions))
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Welcome')
