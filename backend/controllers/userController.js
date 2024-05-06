49878322504c5f3d338834bd279be499990f754c
Siddhant Singh <technosys.sidd@gmail.com>
2024-05-06 21:00:14 +0530

    created the controller to handle logout api call

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,4 +54,16 @@ const registerUser = async(async (req, res) => {
   }
 });
 
+// @desc Logout user / clear cookie
+// @route POST /api/users/logout
+// @access Private
+const logoutUser = async(async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully!" });
+});
+
 export { authUser, registerUser };
